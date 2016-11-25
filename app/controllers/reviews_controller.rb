38db2103e264acac92980aff7a4b04aa912dbb33
Luis Felipe Cortes <lfe.cortes@gmail.com>
2016-11-25 09:11:29 -0200

    Rails Yelp MVP finish

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+  before_action :find_restaurant, only: [:new, :create]
+
+  def new
+    @review = Review.new
+  end
+
+
+  def create
+    @review = @restaurant.reviews.build(review_params)
+    @review.save
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+
+  private
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def review_params
+      params.require(:review).permit(:content, :rating)
+    end
+
+    def find_restaurant
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "55 11 7777 1245",
+    category:     "japanese"
+  },
+  {
+    name:         "Como assim",
+    address:      "Praza calixto",
+    phone_number: "55 11 1234 1850",
+    category:     "french"
+  },
+  {
+    name:         "Kebab",
+    address:      "Teodoro Sampaio",
+    phone_number: "55 11 1234 1245",
+    category:     "italian"
+  },
+  {
+    name:         "McDonalds",
+    address:      "Teodoro Zampaio",
+    phone_number: "55 11 1234 9945",
+    category:     "italian"
+  },
+  {
+    name:         "Burguer king",
+    address:      "Leodoro Sampaio",
+    phone_number: "55 11 1414 1245",
+    category:     "italian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
+
+Restaurant.all.each do |rest|
+  rest.reviews.create(content: "abcd", rating: 5 )
+  rest.reviews.create(content: "xcv", rating: 3 )
+end
